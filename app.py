6c93e3d75adccff86487424afd89843fee1e2799
Reidmen <r.rethmawn@gmail.com>
2025-06-25 20:21:24 -0400

    fixed tts

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ AVAILABLE_MODELS = {
   "Llama-4 Maverick": "meta-llama/llama-4-maverick-17b-128e:free",
 }
 
-def generate_speech(client: openai.OpenAI, text: str | None) -> Path | None: # TTS
+
+def generate_speech(client: openai.OpenAI, text: str | None) -> Path | None:  # TTS
   if not text or text == "" or client.api_key == "":
     return None
   try:
@@ -27,14 +28,17 @@ def generate_speech(client: openai.OpenAI, text: str | None) -> Path | None: # T
     speech_filepath = Path(f"./tmp/audiofile_{unique_id}.mp3")
     print(f"[TTS] Audio file created: {speech_filepath.as_posix()}")
     with client.audio.speech.with_streaming_response.create(
-        model="gpt-4o-mini-tts", voice="alloy", input=text # 
+      model="gpt-4o-mini-tts", # $0.015/min (mp3) speech
+      voice="alloy",
+      input=text, 
     ) as response:
       response.stream_to_file(speech_filepath)
-      
-    return speech_filepath 
+
+    return speech_filepath
   except Exception as e:
     print(f"Error TTS: {e}")
     return None
+
 
 def generate_llm_response(client: openai.OpenAI, text: str, model_name: str, language: str):
   # It assumes the OpenAI - OpenRouter client
@@ -43,52 +47,52 @@ def generate_llm_response(client: openai.OpenAI, text: str, model_name: str, lan
   model_id = AVAILABLE_MODELS.get(model_name, default)
   print(f"Generate LLM request with {model_id=}")
   print(f"{client.base_url}")
-  messages = [
-    {"role": "system", "content": context},
-    {"role": "user", "content": text}
-  ]
+  messages = [{"role": "system", "content": context}, {"role": "user", "content": text}]
   print(f"messages request\n: {messages}")
   # TODO: Llama4 Scout maximum context length of 64000 tokens.
   completion = client.chat.completions.create(
-    extra_headers={},
-    extra_body={},
-    model=model_id,
-    messages=messages,
-    max_completion_tokens=1024 * 4 
+    extra_headers={}, extra_body={}, model=model_id, messages=messages, max_completion_tokens=1024
   )
   response_content = completion.choices[0].message.content
   print(f"LLM: {model_name}\nQuestion: {text}\nResponse: {response_content}")
-  return response_content 
+  return response_content
 
-def transcribe_audio(client:openai.OpenAI, audio_file: str | Path | None): # STT
-  """Transcribe audio with the OpenAI's Whisper API""" 
+
+def transcribe_audio(client: openai.OpenAI, audio_file: str | Path | None):  # STT
+  """Transcribe audio with the OpenAI's Whisper API"""
   if audio_file is None or client.api_key == "":
     return ""
   try:
     with open(audio_file, mode="rb") as audio:
-      transcript = client.audio.transcriptions.create(model="gpt-4o-mini-transcribe", file=audio) # $0.003/min
-      print(f"[STT] Transcription\nText {transcript.text}\n")
+      transcript = client.audio.transcriptions.create(model="gpt-4o-mini-transcribe", file=audio)  # $0.003/min
+      print(f"[STT] Transcription\nText: {transcript.text}\n")
     return transcript.text
 
   except Exception as e:
     print(f"Error STT: {e}")
     return f"Error transcribing audio: {e}"
 
+
 def process_translation(
-    openai_client: openai.OpenAI,
-    openrouter_client: openai.OpenAI,
-    audio_file, typed_text, target_language: str, model_name: str):
+  openai_client: openai.OpenAI,
+  openrouter_client: openai.OpenAI,
+  audio_file,
+  typed_text: str,
+  target_language: str,
+  model_name: str,
+):
   """Handles STT -> LLM -> TTS"""
   print(f"{audio_file=}, {typed_text=}, {target_language=}, {model_name=}")
-  transcribed_text= ""
+  transcribed_text = ""
   if audio_file:
     transcribed_text = transcribe_audio(openai_client, audio_file)
-  input_text = typed_text if typed_text else transcribed_text
+  input_text = transcribed_text if transcribed_text != "" else typed_text 
   if not input_text:
-    return "", "", None # Exit if input_text is ""
+    return "", "", None  # Exit if input_text is ""
   llm_response_text = generate_llm_response(openrouter_client, input_text, model_name, target_language)
   audio_response_path = generate_speech(openai_client, llm_response_text)
   return transcribed_text, llm_response_text, audio_response_path
+
 
 def main(openrouter_api_key: str, openai_api_key: str = ""):
   openrouter_client = openai.OpenAI(api_key=openrouter_api_key, base_url="https://openrouter.ai/api/v1")
@@ -101,19 +105,20 @@ def main(openrouter_api_key: str, openai_api_key: str = ""):
         choices=SUPPORTED_LANGUAGES, value="English", label="Target Language", interactive=True
       )
       model_selector = gradio.Dropdown(
-        choices=list(AVAILABLE_MODELS.keys()),
-        value="Llama-4 Scout",
-        label="LLM (OpenRouter)",
-        interactive=True
+        choices=list(AVAILABLE_MODELS.keys()), value="Llama-4 Scout", label="LLM (OpenRouter)", interactive=True
       )
     with gradio.Row():
       audio_input = gradio.Audio(
         type="filepath", label="Speak your question...", interactive=True, show_download_button=True
       )
-      text_input = gradio.Textbox(label="Or type your question", value="e.g. Can you write a simple poem of the Spanish heritage in the USA? ")
+      text_input = gradio.Textbox(
+        label="Or type your question", value="e.g. Can you write a simple poem of the Spanish heritage in the USA? "
+      )
 
     submit_button = gradio.Button("Process Input")
-    transcribed_audio_ouput = gradio.Textbox(value="Transcribed Text", label="Transcription from OpenAI (Whisper)",interactive=False)
+    transcribed_audio_ouput = gradio.Textbox(
+      value="Transcribed Text", label="Transcription from OpenAI (Whisper)", interactive=False
+    )
     text_response_output = gradio.Textbox(label="LLM Response", interactive=False)
     audio_response_output = gradio.Audio(label="Audio Response", interactive=False)
 
@@ -123,7 +128,7 @@ def main(openrouter_api_key: str, openai_api_key: str = ""):
     submit_button.click(
       fn=_client_translation,
       inputs=[audio_input, text_input, target_language_dropdown, model_selector],
-      outputs=[transcribed_audio_ouput, text_response_output, audio_response_output]
+      outputs=[transcribed_audio_ouput, text_response_output, audio_response_output],
     )
 
   landing_page.launch()
